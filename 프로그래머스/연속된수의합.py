8916df03f2a72ed5870da653c89267b83f15a514
Kuneosu <kks92155784@gmail.com>
2023-01-19 00:02:29 +0900

    solve problem 01.18 (2)

---
+++ 프로그래머스/연속된수의합.py
@@ -0,0 +1,23 @@
+# num = 1
+# total = 0
+
+# center = int(total/num)
+# under_cen = center-1
+# sum = under_cen
+# result = []
+# sum_count = 0
+# while sum != total and sum_count != num: 
+#     if sum+center <= total:
+#         result.append(center)
+#         sum += center
+#         sum_count +=1
+#         center += 1
+#     else :
+#         result.append(under_cen)
+#         sum += under_cen
+#         sum_count += 1
+#         under_cen -= 1
+# result = sorted(result)
+# print(result)
+
+# 못풀겠다.. 내일 다시
