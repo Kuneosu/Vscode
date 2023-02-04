e928c2c586ae1afbc8861adf59768e5786067a76
Kuneosu <kks92155784@gmail.com>
2023-02-04 23:52:02 +0900

    2월4일문제풀이

---
+++ 프로그래머스/문자반복.py
@@ -0,0 +1,6 @@
+s = 'hello'
+n = int(input())
+
+for a in s :
+    for i in range (n):
+        print(a,end="")
---
+++ 프로그래머스/짝수홀수.py
@@ -0,0 +1,10 @@
+num_list = [1,2,3,4,5]
+even = 0
+odd = 0
+for n in num_list:
+    if n%2 == 0:
+       even += 1
+    else :
+        odd += 1
+result =[even,odd]
+print(result) 
