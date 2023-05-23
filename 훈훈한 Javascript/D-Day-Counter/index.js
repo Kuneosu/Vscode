2dd03f59d5a89575ff00fb57f47b8a61f788a2cf
Kuneosu <kks92155784@gmail.com>
2023-05-24 00:15:44 +0900

    훈훈한 Javascript 수강 시작

---
+++ 훈훈한 Javascript/D-Day-Counter/index.js
@@ -0,0 +1,7 @@
+const func = () => {
+    const year = (document.querySelector('#year').value + '년')
+    const month = (document.querySelector('#month').value + '월')
+    const day = (document.querySelector('#day').value + '일')
+
+    console.log(year, month, day)
+}
