a466267490ae4a50560eb354f99d2290fe58e896
Kuneosu <kks92155784@gmail.com>
2023-05-24 22:17:11 +0900

    new Date 파트 수강

--- 훈훈한 Javascript/D-Day-Counter/index.js
+++ 훈훈한 Javascript/D-Day-Counter/index.js
@@ -1,7 +1,22 @@
 const func = () => {
-    const year = (document.querySelector('#year').value + '년')
-    const month = (document.querySelector('#month').value + '월')
-    const day = (document.querySelector('#day').value + '일')
+    const year = (document.querySelector('#year').value)
+    const month = (document.querySelector('#month').value)
+    const day = (document.querySelector('#day').value)
 
     console.log(year, month, day)
+}
+
+const counterMaker = () => {
+    const year = (document.querySelector('#year').value)
+    const month = (document.querySelector('#month').value)
+    const day = (document.querySelector('#day').value)
+
+    const nowDate = new Date();
+    const targetDate = new Date(year + '-' + month + '-' + day);
+    const remaining = (targetDate - nowDate) / 1000;
+    const remainingDate = Math.floor(remaining / 3600 / 24);
+    const remainingHours = Math.floor(remaining / 3600) % 24;
+    const remainingMin = Math.floor(remaining / 60) % 60;
+    const remainingSec = Math.floor(remaining) % 60;
+    console.log(targetDate + " 까지 " + remainingDate + " 일 " + remainingHours + "시간 남았습니다.")
 }
