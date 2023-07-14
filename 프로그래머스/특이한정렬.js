f570dce3e32b75eb75aab619d0a9310d3905868b
Kuneosu <kks92155784@gmail.com>
2023-07-14 15:12:19 +0900

    프로그래머스 문제 풀이

--- 프로그래머스/특이한정렬.js
+++ 프로그래머스/특이한정렬.js
@@ -1,6 +1,15 @@
-const numlist = [1, 2, 3, 4, 5, 6, 7];
+const numlist = [1, 2, 3, 4, 5, 6];
 const n = 4;
-let distance = (numlist.map(x => Math.abs(4 - x))).sort();
-const answer = [];
+const distance = (numlist.map(x => Math.abs(n - x))).sort();
+console.log(distance);
+const answer = distance.map(x => n + x);
+console.log(answer);
+let pre = Math.max(distance) + 1;
+for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === pre || !numlist.includes(answer[i])) {
+        answer[i] -= (answer[i] - n) * 2;
+    }
+    pre = answer[i];
+}
 
-
+console.log(answer);
